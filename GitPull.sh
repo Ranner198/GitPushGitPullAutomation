ba7b028c51316de45f87da5130f1cf05e3b0aedd
John Randall Crump Jr <jrc0051@PhotoLab10.local>
2019-02-06 13:22:01 -0600

    GitPull Automation

---
+++ GitPull.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Functions
+function GitStash {
+	git add .
+	git stash
+	git pull
+	Quit
+}
+
+function GitPull {
+	git add .
+	git commit -m "GitPull Automation"
+	git pull
+	Quit	
+}
+
+function Quit {
+	exit
+}
+
+#Set the color to green
+tput setaf 2;
+
+#Change the terminal name
+echo -en "\033]0;GitPull v0.01 Ran Crump\a"
+
+echo "Git Stash?"
+read Stash
+
+if ("$Stash" == "y"); then
+  GitStash
+else
+  GitPull
+fi
---
+++ GitPush.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Functions
+function PushComment {
+	git add .
+	git commit "{$Input}"
+	git pull
+	git push
+	Quit
+}
+
+function PushEmpty {
+	git add .
+	git commit -a --allow-empty-message -m ''
+	git pull
+	git push
+	Quit	
+}
+
+function Quit {
+	exit
+}
+
+#Set the color to green
+tput setaf 2;
+
+#Change the terminal name
+echo -en "\033]0;GitPull v0.01 Ran Crump\a"
+
+echo "Insert a commit message or leave blank:"
+read Input
+
+if ("$Input" != "" && "$Input" != " "); then
+  PushComment
+else
+  PushEmpty
+fi
