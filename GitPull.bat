72da207c1ff7cc01c88378cf48bbcb846e296c6c
Ran Crump <rancrump18@gmail.com>
2019-10-22 19:52:38 -0500

    new gui

--- GitPull.bat
+++ GitPull.bat
@@ -1,8 +1,23 @@
 @echo off
 
 color 0a
-title GitPull V0.01 - Ran Crump
+title GitPull V1.0 - Ran Crump
 
-set PATH=%PATH%;C:\Program Files\Git\cmd
+rem set PATH=%PATH%;C:\Program Files\Git\cmd
+
+SET /P input=Would you like to stash first? y/n:
+
+IF "%input%"=="y" GOTO :stash 
+IF "%input%"=="Y" GOTO :stash
+
+GOTO :pull
+
+:stash
+git add .
+git stash
+
+
+:pull
 git pull
+git status
 pause
